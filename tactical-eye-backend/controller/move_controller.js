1244bda8189ac437aaa1ff93a9ad8fad46f9cb7a
paras <vivekupadhayay18@gmail.com>
2025-03-06 19:53:27 +0530

    update /api

--- tactical-eye-backend/controller/move_controller.js
+++ tactical-eye-backend/controller/move_controller.js
@@ -16,11 +16,12 @@ function getMoveCoordinates(fen, algebraicMove) {
 const moveController = {
 
     makeMove: async (req, res) => {
-        const fenstr = req.body.fenstring.split(" ").slice(-6).join(" ");
+        var fenstr = req.body.fenstring;
         if (!fenstr) {
             res.status(400).json({ message: "Fenstring is required" });
             return;
         }
+        fenstr = fenstr.split(" ").slice(-6).join(" ");
         const pythonScriptPath = path.resolve(__dirname, "../chess-engine/Sepentia-ChessEngine/src/GameHelper.py");
         const result = await pythonTools.executePython(pythonScriptPath, [fenstr])
         if (!result) {
