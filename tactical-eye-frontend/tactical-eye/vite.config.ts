0bc785247aa27a07469eb56d33899920a5a35262
paras <vivekupadhayay18@gmail.com>
2025-03-08 00:29:11 +0530

    fix: vite.config.js

--- tactical-eye-frontend/tactical-eye/vite.config.ts
+++ tactical-eye-frontend/tactical-eye/vite.config.ts
@@ -3,8 +3,6 @@ import {config} from "dotenv";
 import react from '@vitejs/plugin-react'
 
 config();
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
